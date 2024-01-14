c5813da10d130cd3db07278c9a5271b199851a8b
Kenny <tehboonkheng1997@hotmail.com>
2024-01-14 13:52:48 +0800

    backup

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class loginform(forms.Form):
     username = forms.CharField(min_length=8, max_length=25, strip=True)
-    password = forms.CharField(min_length=8, max_length=18, widget=forms.PasswordInput)
+    password = forms.CharField(min_length=8, max_length=18, widget=forms.PasswordInput(render_value=False))
     
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import loginform
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,3 +10,10 @@ def sign_in(request):
     if request.method == 'GET':
         form = loginform()
         return render(request, 'users/loginpage.html', {'form':form})
+    elif request.method == 'POST':
+        form = loginform(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,)
