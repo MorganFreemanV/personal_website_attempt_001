dcb814b55acc5c01f97dd2fba4d6acf66762e574
Kenny <tehboonkheng1997@hotmail.com>
2024-04-06 14:53:11 +0800

    attempt connectfour

--- Django Udemy Tutorial/DOM/connectfour.js
+++ Django Udemy Tutorial/DOM/connectfour.js
@@ -1,9 +1,11 @@
-var current_color = '';
-var heights = document.querySelectorAll('tr');
-var widths = heights[0].querySelectorAll('td');
+var current_color = "";
+var heights = document.querySelectorAll("tr");
+var widths = heights[0].querySelectorAll("#circle");
 
-for (var i = 0; i < heights.length; i++) {
-    for (var j = 0; j < widths.length; j++) {
-        widths[j].addEventListener('click', console.log('Clicked!', i, j));
-    };
-};
+for (var j = 0; j < widths.length; j++) {
+  widths[j].addEventListener("click", changecontent(widths[j]));
+}
+
+function changecontent(width) {
+  width.style.color = "blue";
+}
