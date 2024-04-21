d50d9d471f51dfe9725e2cd756ef05b06eff1b26
Kenny <tehboonkheng1997@hotmail.com>
2024-04-21 19:53:25 +0800

    much work, no progress

--- Django Udemy Tutorial/DOM/connectfour.js
+++ Django Udemy Tutorial/DOM/connectfour.js
@@ -1,6 +1,6 @@
 var current_color = "";
 var heights = document.querySelectorAll("tr");
-var widths = heights[0].querySelectorAll("#circle");
+var widths = heights[0].querySelectorAll("span.circle");
 
 for (var j = 0; j < widths.length; j++) {
   widths[j].addEventListener("click", changecontent(widths[j]));
