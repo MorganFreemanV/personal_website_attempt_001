e98788098123706b0cc14e7a66f14965fa9368a0
Kenny <tehboonkheng1997@hotmail.com>
2024-05-26 01:02:23 +0800

    added checkrow, addrow, and various useful comments

--- Django Udemy Tutorial/DOM/connectfour.js
+++ Django Udemy Tutorial/DOM/connectfour.js
@@ -1,24 +1,61 @@
+// player 1 prompt, color is blue
 var player1 = prompt("What is your name? You will be blue");
-$("#player1").attr("id", player1);
-var player1color = "blue";
-var player2 = prompt("What is your name? You will be red");
-var player2color = "red";
-$("#player2").attr("id", player2);
-var turn = 1;
+$("#player1").text(player1);
+var player1color = "rgb(0, 0, 255)";
 
+// player 2 prompt, color is red
+var player2 = prompt("What is your name? You will be red");
+var player2color = "rgb(255, 0, 0)";
+$("#player2").text(player2);
+
+// initiate turn
+var turn = 1;
+// assign variable table to tr, table rows
+table = $("table tr");
+
+// on button click, run the below sequence of code
 $("button").on("click", function () {
+  // find column number
   var col = $(this).closest("td").index();
-  var row = $(this).closest("tr").index();
-  var curplayer = checkplayer();
-  // function to check player
-  // function to check row  to drop down to
-  // function to check connectfour
+
+  // function to check current player color
+  var curplayercolor = checkplayer();
+
+  // function to check row to drop down to
+  var row = checkrow(curplayercolor, col);
+
+  // function to drop down color
+  addrow(row, col, curplayercolor);
+  turn = turn * -1;
+
+  // function to check if connected four
   // function to check wincon
-  console.log("col number is: " + col);
-  console.log("row number is: " + row);
 });
 
 function checkplayer() {
-  if (turn === 1) return player1;
-  else return player2;
+  if (turn === 1) return player1color;
+  else return player2color;
+}
+
+function checkrow(curplayercolor, col) {
+  for (var rows = 6; rows > -1; rows--) {
+    var colorreport = table
+      .eq(rows)
+      .find("td")
+      .eq(col)
+      .find("button")
+      .css("background-color");
+    if (colorreport === "rgb(255, 228, 196)") {
+      return rows;
+    }
+  }
+}
+
+function addrow(row, col, curplayercolor) {
+  table
+    .eq(row)
+    .find("td")
+    .eq(col)
+    .find("button")
+    .css("background-color", curplayercolor);
 }
