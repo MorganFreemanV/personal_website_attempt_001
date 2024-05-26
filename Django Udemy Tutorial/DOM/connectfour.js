66201670e466c9ca5d982b8da96665314480dcc2
Kenny <tehboonkheng1997@hotmail.com>
2024-05-26 23:05:19 +0800

    added horizontal win con

--- Django Udemy Tutorial/DOM/connectfour.js
+++ Django Udemy Tutorial/DOM/connectfour.js
@@ -1,12 +1,16 @@
 // player 1 prompt, color is blue
 var player1 = prompt("What is your name? You will be blue");
-$("#player1").text(player1);
+if (player1) {
+  $("#player1").text(player1);
+}
 var player1color = "rgb(0, 0, 255)";
 
 // player 2 prompt, color is red
 var player2 = prompt("What is your name? You will be red");
 var player2color = "rgb(255, 0, 0)";
-$("#player2").text(player2);
+if (player2) {
+  $("#player2").text(player2);
+}
 
 // initiate turn
 var turn = 1;
@@ -29,6 +33,8 @@ $("button").on("click", function () {
   turn = turn * -1;
 
   // function to check if connected four
+  checkhorizontal();
+  /* var wincount = Math.max(checkhorizontal(), checkvertical(), checkdiagonal()); */
   // function to check wincon
 });
 
@@ -58,4 +64,55 @@ function addrow(row, col, curplayercolor) {
     .eq(col)
     .find("button")
     .css("background-color", curplayercolor);
+}
+
+// long version
+function checkhorizontal() {
+  var count = 0;
+  var curcolor = "";
+  for (var row = 0; row < 7; row++) {
+    for (var col = 0; col < 6; col++) {
+      var color_placeholder = table
+        .eq(row)
+        .find("td")
+        .eq(col)
+        .find("button")
+        .css("background-color");
+      console.log(row + ", " + col + " " + color_placeholder);
+      if (col === 0) {
+        curcolor = color_placeholder;
+        count = count + 1;
+        console.log(
+          "initialise: cur background colour: " +
+            curcolor +
+            " current count: " +
+            count
+        );
+      } else if (
+        color_placeholder === curcolor &&
+        curcolor !== "rgb(255, 228, 196)"
+      ) {
+        count = count + 1;
+        console.log("count increased to " + count);
+        console.log(color_placeholder + " " + curcolor);
+      } else {
+        count = 1;
+        curcolor = color_placeholder;
+        console.log("reset");
+      }
+      if (count >= 4) {
+        console.log(curcolor + " win!");
+        reportwin(curcolor);
+      }
+    }
+    count = 0;
+  }
+}
+
+function reportwin(curcolor) {
+  if (curcolor === player1color) {
+    alert($("#player1").text() + " wins!");
+  } else {
+    alert($("#player2").text() + " wins!");
+  }
 }
