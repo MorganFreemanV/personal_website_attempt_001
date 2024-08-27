764489e59c71c9e9dde2b7dffc21d08e6855147c
Kenny <tehboonkheng1997@hotmail.com>
2024-08-27 16:54:30 +0800

    relaunching django project

--- mysite/users/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class loginform(forms.Form):
-    username = forms.CharField(min_length=8, max_length=25, strip=True)
-    password = forms.CharField(min_length=8, max_length=18, widget=forms.PasswordInput(render_value=False))
-    
---
+++ venv_cheatsheet.py
@@ -0,0 +1,15 @@
+
+# to activate, run below in PS
+'''
+make sure path is as below
+C:\Users\tehbo\OneDrive\Desktop\TOP SECRET\Programming\personal_website_attempt_001\mysite
+.venv/Scripts/activate.ps1 
+'''
+# to deactivate, run below in PS
+'''
+deactivate
+'''
+
+# cehck python executable path
+import sys
+print(sys.executable)
