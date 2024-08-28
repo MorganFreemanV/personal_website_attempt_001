9a992e6a0faeca54032b61b783e96e40d80831da
Kenny <tehboonkheng1997@hotmail.com>
2024-08-28 17:15:29 +0800

    recommitting as previous commit filepath only includes mysite

--- venv_cheatsheet.py
+++ venv_cheatsheet.py
@@ -3,7 +3,7 @@
 '''
 make sure path is as below
 C:\Users\tehbo\OneDrive\Desktop\TOP SECRET\Programming\personal_website_attempt_001\mysite
-.venv/Scripts/activate.ps1 
+venv/Scripts/activate.ps1 
 '''
 # to deactivate, run below in PS
 '''
