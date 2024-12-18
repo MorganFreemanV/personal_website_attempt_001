6884cbc5ec4ce0356a6ee9987b4234fa6e1b4e34
Teh Boon Kheng <tkheng@kryptonfs.com>
2024-12-18 17:22:58 +0800

    forgot to update progress on login, recommitting

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from register.forms import UserForm, UserProfileInfoForm
 
+#for logins
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'homepage.html')
 
 def register_page(request):
     registered = False
@@ -28,3 +37,18 @@ def register_page(request):
         profile_form = UserProfileInfoForm()
 
     return render(request, 'register/register_page.html', {'user_form':user_form, 'profile_form':profile_form, 'registered':registered}, )
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username="username", password="password",)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return HttpResponse("Account not active!")
+        else:
+            print("Someone tried to login and failed!")
+            print(f"Username: {username} , Password: {password}")
